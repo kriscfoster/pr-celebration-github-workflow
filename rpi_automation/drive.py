06b15184ca25d6a49155bcd740f9ffafee4cbbee
kriscfoster <krisfoster@live.ie>
2021-11-25 08:05:29 +0000

    debug

--- rpi_automation/drive.py
+++ rpi_automation/drive.py
@@ -10,6 +10,7 @@ drive_folder_id=os.environ.get('DRIVE_FOLDER_ID')
 def upload_file(file_path):
   print("uploading file")
   gfile = drive.CreateFile({"parents": [{"id": drive_folder_id}]})
+  print(gfile)
   # print(file_path)
   # gfile.SetContentFile("/home/pi/celebration_images/1637827012932.jpg")
   gfile.Upload()
