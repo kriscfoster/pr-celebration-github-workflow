c96c1843816e596404c4f48d48d0bae4cca75bd3
kriscfoster <krisfoster@live.ie>
2021-11-25 07:27:12 +0000

    test

--- rpi_automation/picture.py
+++ rpi_automation/picture.py
@@ -10,6 +10,6 @@ def take_picture():
   millis=str(round(time.time() * 1000))
   celebration_image_path=celebration_images_base_path + millis + '.jpg'
   print("taking picture: " + celebration_image_path)
-  camera.capture(celebration_image_path)
+  camera.capture(celebration_image_path, use_video_port=True)
   print("picture successfully taken")
   return celebration_image_path
